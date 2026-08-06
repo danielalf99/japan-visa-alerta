e7ec5d439a75d95ce017f80bc8b902fc6bf224c2
danielalf99 <danielalegfe@gmail.com>
2026-08-05 19:00:50 -0500

    Update bot.py
    Crear bot inicial

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://embjpcol.rsvsys.jp/reservations/calendar"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
+}
+
+response = requests.get(
+    URL,
+    headers=headers,
+    timeout=30
+)
+
+print("Estado de la página:", response.status_code)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+texto = soup.get_text(" ", strip=True)
+
+print(texto[:5000])
