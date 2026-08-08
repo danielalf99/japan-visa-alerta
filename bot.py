ab73caf9d0b2af32360bf5db5ee05909031d551c
danielalf99 <danielalegfe@gmail.com>
2026-08-08 09:42:24 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-¿from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright
 
 URL = "https://embjpcol.rsvsys.jp/reservations/calendar"
 
@@ -6,30 +6,19 @@ with sync_playwright() as p:
 
     browser = p.chromium.launch(headless=True)
 
-    page = browser.new_page(
-        user_agent=(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/150.0.0.0 Safari/537.36"
-        )
-    )
+    page = browser.new_page()
 
-    print("Abriendo calendario...")
+    print("Abriendo la página...")
 
-    page.goto(
-        URL,
-        wait_until="networkidle",
-        timeout=60000
-    )
+    page.goto(URL, wait_until="networkidle", timeout=60000)
 
-    print("Página cargada")
+    print("Página cargada.")
 
-    # Esperar a que el calendario aparezca
     page.wait_for_timeout(5000)
 
     texto = page.locator("body").inner_text()
 
-    print("\n--- CALENDARIO ---")
+    print("----- INFORMACIÓN DEL CALENDARIO -----")
     print(texto[:10000])
 
     browser.close()
